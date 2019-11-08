e4bb808f5190bd1d21d644fa04a7dea7f9607ba8
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-08 02:35:30 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,15 +11,18 @@ def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  SELECT bears.name , bear.age FROM bears MAX (age);
+  SELECT bears.name , bears.age FROM bears MAX (age);
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  SELECT bears.name , bears.age FROM bears MIN (age);
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+SELECT color AS most_frequent_value
+FROM bears
+GROUP BY color
+ORDER BY COUNT(*) DESC LIMIT 1;
 end
 
 def counts_number_of_bears_with_goofy_temperaments
